d28025507e80e89b80d68b9dd543821b27768dfc
rosannadeng <rosannadeng@gmail.com>
2024-01-21 22:39:52 -0500

    Modified specific shape creation under Shape; add getArea in Shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,17 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;  // Import the correct Shape interface
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;  // Assuming Circle is also in this package
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(4);
 
         Renderer renderer = new Renderer(rectangle);
-
+        Renderer renderer2 = new Renderer(circle);
         renderer.draw();
+        renderer2.draw();
     }
 }
